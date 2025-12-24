0251b777c7933d59f48df185daa36dd2f813b044
Baibhav Pratap Singh <b2p16s19@gmail.com>
2025-12-25 00:46:53 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require("dotenv").config({path: "./src/config/.env"});
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
@@ -8,7 +9,26 @@ dotenv.config({
 });
 
 
-connectDB()
+
+connectDB()                    // 1. Try to connect to MongoDB
+.then(() => {                  // 2. If connection succeeds
+    app.on("error", (error) => {
+      console.error("Error:", error);
+      throw error;
+    });                        // 3. Set up error listener for Express app
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is listening at PORT: ${process.env.PORT || 8000}`)
+    })                          // 4. Start the server
+})
+.catch((error) => {            // 5. If connection fails
+    console.log(`MongoDB connection failed !!!`, error)         // 6. Log the error
+})
+
+
+
+
+
+
 
 
 
