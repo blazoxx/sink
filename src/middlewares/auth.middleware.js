a63f9073ae09c6745995d0ea64b6e66154019070
Baibhav Pratap Singh <b2p16s19@gmail.com>
2025-12-29 12:11:02 +0530

    logout funcn made

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,16 +3,28 @@ import { ApiError } from "../utils/ApiError";
 import jwt from "jsonwebtoken";
 import { User } from "../models/User.model.js";
 
-export const verifyJWT = asyncHandler(async (req, res, next) => {
-  const token =
-    req.cookies?.accessToken ||
-    req.headers("Authorization")?.replace("Bearer ", "");
-
-  if (!token) {
-    throw new ApiError(401, "Unauthorized request: No token provided");
+export const verifyJWT = asyncHandler(async (req, _, next) => {  //if res is not used, replace it with _ to avoid linting errors
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers("Authorization")?.replace("Bearer ", "");
+  
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request: No token provided");
+    }
+  
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  
+    const user = await User.findById(decodedToken._id).select("-password -refreshToken");
+  
+    if(!user) {
+      throw new ApiError(401, "Invalid token: User does not exist");
+    }
+  
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid or expired token");
   }
 
-  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-  await User.findById(decodedToken._id)
 });
